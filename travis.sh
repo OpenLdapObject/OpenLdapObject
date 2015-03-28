946c196e74a05ec6e1cfb014357916aa1f695e5b
Pierre Pélisset <ppelisset@hotmail.fr>
2015-03-28 20:09:07 +0100

    Update travis

--- travis.sh
+++ travis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-/opt/apacheds-2.0.0-M19/bin/apacheds start default
+sudo /opt/apacheds-2.0.0-M19/bin/apacheds start default
 
 sleep 10
 
-ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
-ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
-ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
-ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
+sudo ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
+sudo ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
+sudo ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
+sudo ldapadd -c -H ldap://localhost:10389 -x -D "uid=admin,ou=system" -f tests/docker-test/data.ldif -w secret
 
 composer install;
 mv tests/OpenLdapObject/Tests/TestConfiguration.php tests/OpenLdapObject/Tests/TestConfiguration.php.origin;
